62ec4a52d8213d0bc09d691991d5bf96b08fc200
Hoang Nguyen <minh.hoang1297@gmail.com>
2020-03-30 15:01:38 -0400

    Update comments

--- ext2_ls.c
+++ ext2_ls.c
@@ -19,38 +19,37 @@ int main(int argc, char **argv) {
     fprintf(stderr, "Usage: ./ext2_ls <image file path> [-a] <absolute path to list>\n");
     exit(1);
   }
+  // check for options
   int all_option = 0;
   char *ls_path = argv[2];
   if (strcmp(argv[2], "-a") == 0) {
     all_option = 1;
     ls_path = argv[3];
   }
+  // load disk and perform path walk
   loaddisk(argv[1]);
   struct ext2_dir_entry_2 *dir_entry_to_ls;
   struct ext2_inode *inode_to_ls = path_walk(ls_path, &dir_entry_to_ls);
+  // check type of inode, and print accordingly
   if (inode_to_ls == NULL) {
     printf("No such file or directory\n");
     return ENOENT;
   } else if (get_type_inode(inode_to_ls) != 'd') {
     printf("%.*s\n", dir_entry_to_ls->name_len, dir_entry_to_ls->name);
   } else {
-    // Get the array of blocks from inode
+    // if it's a dir, get the array of blocks, and loop through all entries,
+    // in all blocks, printing accordingly
     unsigned int *arr = inode_to_ls->i_block;
     while (*arr != 0) {
-      // get starting position in the block;
       unsigned long pos = (unsigned long)disk + *arr * EXT2_BLOCK_SIZE;
       struct ext2_dir_entry_2 *dir = (struct ext2_dir_entry_2 *)pos;
-      // loop till the end of the block
       do {
-        // if given name match current inode name, return current inode
         if (all_option || !isCurOrPrev(dir->name, dir->name_len)) {
           printf("%.*s\n", dir->name_len, dir->name);
         }
-        // advance to the next inode
         pos += dir->rec_len;
         dir = (struct ext2_dir_entry_2 *)pos;
       } while (pos % EXT2_BLOCK_SIZE != 0);
-      // advance to the next block in array
       arr++;
     }
   }
