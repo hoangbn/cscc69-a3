0fd863c5e7d04e537bb721c337aab6e38def04a6
Hoang Nguyen <minh.hoang1297@gmail.com>
2020-04-06 19:25:30 -0400

    Added missing cases for ln

--- ext2_ln.c
+++ ext2_ln.c
@@ -16,6 +16,7 @@ int main(int argc, char **argv) {
     exit(1);
   }
   loaddisk(argv[1]);
+  print_disk_image(); // TODO: remove
 
   char *target;
   char *dest;
@@ -38,13 +39,10 @@ int main(int argc, char **argv) {
     dest = argv[4];
     target_ft = EXT2_FT_SYMLINK;
 }
-
-  char *dest_filename = basename(dest);
-
   // Check that target is a file (not a directory)
   struct ext2_dir_entry_2 *target_dir_entry;
   struct ext2_inode *target_inode = path_walk(target, &target_dir_entry);
-
+  char *orig_filename = basename(target);
   // Check that given target file is valid and not a directory
   if (target_inode == NULL) {
     return ENOENT;
@@ -57,16 +55,38 @@ int main(int argc, char **argv) {
   // Perform path walk to directory one above destination path
   struct ext2_inode *second_last = path_walk_second_last(
             dest, &last_section_name, &second_last_inodenum);
-
+  // validate if given destination path was valid
+  if (second_last == NULL) {
+    printf("No such file or directory\n");
+    return ENOENT;
+  }
+  char* des_filename;
+  struct ext2_inode *des_dir = second_last;
+  // if destination is root
+   if (last_section_name == NULL) des_filename = orig_filename;
+  // validate the last_section_name: it could either be a directory or empty
+  // if empty: we want to cp file to second_last_dir with new file name last_section_name
+  // if it's a directory: we want to copy to that directory with same file name
+  else {
+    struct ext2_inode *last_inode = get_next_inode(second_last, last_section_name, NULL);
+    if (last_inode == NULL) des_filename = last_section_name;
+    else if (get_type_inode(last_inode) == EXT2_FT_DIR) {
+      des_dir = last_inode;
+      des_filename = orig_filename;
+    } else {
+      printf("File/link with that name already exists\n");
+      exit(EEXIST);
+    }
+  }
   // Create hard link
   if (target_ft == EXT2_FT_REG_FILE) {
     // Create new directory entry pointing to original target's inode (target_dir_entry->inode)
-    create_dir_entry(second_last, dest_filename, EXT2_FT_REG_FILE, target_dir_entry->inode);
+    create_dir_entry(des_dir, des_filename, EXT2_FT_REG_FILE, target_dir_entry->inode);
   }
   // Create symbolic link
   else {
     struct ext2_dir_entry_2 *new_dir_entry = create_dir_entry(
-              second_last, dest_filename, EXT2_FT_SYMLINK, 0);
+              des_dir, des_filename, EXT2_FT_SYMLINK, 0);
 
     // Data block for new inode should contain the path to the original file (target)
     struct ext2_inode *new_inode = get_inode(new_dir_entry->inode);
@@ -85,6 +105,6 @@ int main(int argc, char **argv) {
     // Copy the target path into the block
     memcpy(block, target, target_path_len);
   }
-
+  print_disk_image(); // TODO: remove
   return 0;
 }
